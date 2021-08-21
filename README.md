6358e8ba4406e2c088383f01af0f8709ffd663bd
Sangkook Lee <sangkook.lee@solarsdev.com>
2021-08-21 13:11:03 +0900

    day 2

---
+++ README.md
@@ -0,0 +1,6 @@
+# Daily algorithm practice
+
+## BOJ (Beakjoon Online Judge)
+
+- 백준 온라인 저지에서 제출된 문제를 풀이
+- Node.js를 이용
---
+++ boj/10869/solve.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
