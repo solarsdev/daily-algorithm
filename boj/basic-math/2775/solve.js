ec04e81047f9b1a259d7a9ef0f9fe7e5dffb35c8
Sangkook Lee <sangkook.lee@solarsdev.com>
2021-09-06 18:45:22 +0900

    day 12

--- boj/basic-math/2775/solve.js
+++ boj/basic-math/2775/solve.js
@@ -7,13 +7,24 @@ const t = parseInt(input[0]);
 // 1 3  6 10 15
 // 1 2  3  4  5
 
-// 케이스 입력
-for (let i = 1; i < t * 2 + 1; i += 2) {
-  const k = input[i];
-  const n = input[i + 1];
-
-  let rooms = new Array(n + 1);
-  for (let j = 1; j < n + 1; j++) {
-    rooms[j] = j;
+const getResidenceNum = (k, n) => {
+  if (k === 0) {
+    return n;
   }
+  if (n === 1) {
+    return 1;
+  }
+  return getResidenceNum(k - 1, n) + getResidenceNum(k, n - 1);
+};
+
+// 케이스 입력 부분
+let ret = '';
+
+for (let i = 1; i < t * 2 + 1; i += 2) {
+  const k = parseInt(input[i]);
+  const n = parseInt(input[i + 1]);
+
+  ret += getResidenceNum(k, n) + '\n';
 }
+
+console.log(ret.trim());
